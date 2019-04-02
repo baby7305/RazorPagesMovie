fb9b3815c521acbad0d10cc9d16b235ad9d2d83d
baby7305 <songhaibei7305@163.com>
2019-04-02 20:20:28 +0800

    格式化Startup

--- Startup.cs
+++ Startup.cs
@@ -9,47 +9,37 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace RazorPagesMovie0402
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace RazorPagesMovie0402 {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
+        public void ConfigureServices (IServiceCollection services) {
+            services.Configure<CookiePolicyOptions> (options => {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler ("/Error");
             }
 
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+            app.UseStaticFiles ();
+            app.UseCookiePolicy ();
 
-            app.UseMvc();
+            app.UseMvc ();
         }
     }
 }

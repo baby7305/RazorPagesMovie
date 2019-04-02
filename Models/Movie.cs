6712ac83837cdeed1e6d6f7afcd1bb8420590a6b
baby7305 <songhaibei7305@163.com>
2019-04-02 21:46:09 +0800

    限制数据存储格式，18位数字，4位小数

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models {
     public class Movie {
@@ -10,6 +11,8 @@ namespace RazorPagesMovie.Models {
         [Display (Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column (TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }

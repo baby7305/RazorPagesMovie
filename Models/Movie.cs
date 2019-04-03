6c95471aebad522aae9dd634f4581f890915e351
baby7305 <songhaibei7305@163.com>
2019-04-04 01:45:36 +0800

    Rating的验证在页面上也显示出来

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,26 +7,19 @@ namespace RazorPagesMovie.Models {
         public int ID { get; set; }
 
         [StringLength (60, MinimumLength = 3)]
-        [Required]
         public string Title { get; set; }
 
-        [DataType (DataType.Date)]
-        [Display (Name = "Release Date")]
+        [Display (Name = "Release Date"), DataType (DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        [RegularExpression (@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Required]
-        [StringLength (30)]
+        [RegularExpression (@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength (30)]
         public string Genre { get; set; }
 
-        [Range (1, 100)]
-        [DataType (DataType.Currency)]
+        [Range (1, 100), DataType (DataType.Currency)]
         [Column (TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
-        [RegularExpression (@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength (5)]
-        [Required]
+        [RegularExpression (@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength (5)]
         public string Rating { get; set; }
     }
 }
